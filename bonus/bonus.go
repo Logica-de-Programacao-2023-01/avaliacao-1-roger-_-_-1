f614fc4ab2b0dbece3aa61fb37a018b33b73c1f6
Moeeru <126564425+Moeeru@users.noreply.github.com>
2023-04-25 10:06:04 -0300

    Update bonus.go

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -5,8 +5,21 @@ package bonus
 //
 //Joãozinho quer construir o menor número possível de torres com as barras que tem. Você deve ajudar Joãozinho a usar as
 //barras da melhor maneira possível, determinando a altura da torre mais alta e quantas torres podem ser construídas.
-
 func CalculateTowers(barLengths []int) (int, int) {
-	// Seu código aqui
-	return 0, 0
+	altmax:= 0
+	ntorre:= 0
+	for i :=0; i <len(barLengths);i++{
+		altura:=0
+		ndestaque:=0
+		for j := i; j<len(barLengths)|| barLengths[i]==barLengths[j];j++{
+			altura++
+			ndestaque++
+		}
+		if altura > altmax && altura>1{
+			altmax= altura
+		}
+		ntorre++
+		i+= ndestaque
+	} 
+	return altmax, ntorre
 }
