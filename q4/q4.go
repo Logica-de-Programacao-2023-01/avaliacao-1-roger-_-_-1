7646a6e9fe4ac819270dae1040f86d75379e4196
Moeeru <126564425+Moeeru@users.noreply.github.com>
2023-04-25 09:59:31 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -14,10 +14,13 @@ func ClassifyPrices(prices []int) (int, error) {
 	if len(prices) == 0 {
 		return 0, fmt.Errorf("erro")
 	}
+	if len(prices)==1{
+		return 3,nil
+	}
 	crescente := 0
 	decrescente := 0
-	for i := 0; i < len(prices)-1; i++{
-		if prices[i] >= prices[i+1] {
+	for i := 0; i < len(prices)-1; i++ {
+		if prices[i] > prices[i+1] {
 			crescente++
 		} else if prices[i] < prices[i+1] {
 			decrescente++
