60a85ee534e1dd5415cd826701f449e8d9119b26
ItzTutu <104566356+tutupro@users.noreply.github.com>
2023-04-25 09:20:18 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -8,6 +8,7 @@ package q4
 //ordem crescente, decrescente ou aleatória. A função deve retornar 1 se a lista estiver em ordem crescente, 2 se a lista
 //estiver em ordem decrescente e 3 se a lista estiver aleatória. A função deve retornar um erro se a lista estiver vazia.
 //Caso a lista possua apenas um elemento, a função deve retornar 3.
+import "fmt"
 
 func ClassifyPrices(prices []int) (int, error) {
 	if len(prices) == 0 {
@@ -23,6 +24,6 @@ func ClassifyPrices(prices []int) (int, error) {
 
 		}
 	}
-	return 0, nil
 
+return 0, nil
 }
